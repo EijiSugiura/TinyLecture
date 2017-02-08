2a45fc3cd1d7c3c153833553694ae531ec2306d2
Eiji Sugiura <eiji.sugiura@gmail.com>
2017-02-08 17:30:40 +0900

    Add name/rev counter

---
+++ perl/daily-top10-rev.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use strict;
+
+opendir(my $DIR,".");
+foreach my $filename ( sort( grep(/.*\.csv.xz$/, readdir($DIR) ) ) ){
+    my %counter;
+    open(my $FILE,"xz -dc $filename |");
+    while(my $line = <$FILE>) {
+	my ($name,$rev) = (split(/,/, $line))[6,9];
+	++$counter{$name.','.$rev};
+    }
+    close($FILE);
+    print "$filename\n";
+    my @names = sort {$counter{$b} <=> $counter{$a}} keys(%counter);
+    foreach my $name (splice(@names, 0, 10)) {
+	print "$name,$counter{$name}\n";
+    }
+}
+closedir($DIR);
+exit(0);
